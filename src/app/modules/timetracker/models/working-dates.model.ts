21a2194f0665b4269484454eb613211150120444
Alejandro Reyes <areyes@VISIONWEB.COM>
2021-07-05 13:12:52 -0600

    Añadiendo catalogos de Projectos y Días de Trabajo

---
+++ src/app/modules/timetracker/models/working-dates.model.ts
@@ -0,0 +1,7 @@
+import { CalendarEvent } from 'angular-calendar';
+import { BDMetaData } from './bd-metadata.model';
+
+export interface NoneWorkingDateData {
+  description: number;
+  date: CalendarEvent;
+}
